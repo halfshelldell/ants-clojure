9137ae635bc9df25221ac7eec752bf77e20c70dc
halfshelldell <simmonslydell@gmai.com>
2016-07-14 12:13:16 -0400

    add move ants

--- src/ants_clojure/core.clj
+++ src/ants_clojure/core.clj
@@ -18,17 +18,42 @@
     (.setFill context javafx.scene.paint.Color/BLACK)
     (.fillOval context (:x ant) (:y ant) 5 5)))
 
+(defn move-ant [ant]
+  (Thread/sleep 1)
+  (let [x-change (- (* 2 (rand)) 1)
+        y-change (- (* 2 (rand)) 1)]
+    (assoc ant 
+      :x (+(:x ant) x-change)
+      :y (+ (:y ant) y-change))))
+
+(defn move-ants []
+  (pmap move-ant @ants))
+
+(def last-timestamp (atom 0))
+
+(defn fps [current-timestamp]
+  (let [diff (- current-timestamp @last-timestamp)
+        diff-seconds (/ diff 1000000000)]
+    (int (/ 1 diff-seconds))))
+
 (defn -start [app stage]
   (let [root (javafx.fxml.FXMLLoader/load (io/resource "main.fxml"))
         scene (javafx.scene.Scene. root width height)
         canvas (.lookup scene "#canvas")
-        context (.getGraphicsContext2D canvas)]
+        context (.getGraphicsContext2D canvas)
+        fps-label (.lookup scene "#fps")
+        timer (proxy [javafx.animation.AnimationTimer] []
+                (handle [now]
+                  (reset! ants (move-ants))
+                  (draw-ants! context)
+                  (.setText fps-label (str (fps now)))
+                  (reset! last-timestamp now)))]
     (.setTitle stage "Ants")
     (.setScene stage scene)
     (.show stage)
     (reset! ants (create-ants))
-    (draw-ants! context)))
-
+    (.start timer)))
+    
 
 (defn -main []
   (javafx.application.Application/launch ants_clojure.core (into-array String [])))
